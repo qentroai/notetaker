50b0bdd13108cbe69ec88a3290ec4de5d5c58aa0
qentroai <qentro.startup@gmail.com>
2026-08-11 22:51:02 -0600

    add client ID in config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
-  GOOGLE_CLIENT_ID: "YOUR_CLIENT_ID.apps.googleusercontent.com",
+  GOOGLE_CLIENT_ID: "386349028859-pesqn117k4g4g97r9a1cvlcti49b6jtb.apps.googleusercontent.com",
   DRIVE_FOLDER_NAME: "Meeting Notes"
 };
